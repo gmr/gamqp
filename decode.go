daee7ee03f5673118036516604bf043daa32b575
Gavin M. Roy <gavinmroy@gmail.com>
2015-08-13 00:08:08 -0400

    Implement more decode functions and tests

--- decode.go
+++ decode.go
@@ -21,7 +21,16 @@ func decodeBoolean(r io.Reader) (value bool, err error) {
 }
 
 func decodeByteArray(r io.Reader) (value []byte, err error) {
-	return
+	var length uint32
+	err = binary.Read(r, binary.BigEndian, &length)
+	if err != nil {
+		return
+	}
+	bytes := make([]byte, length)
+	if _, err = io.ReadFull(r, bytes); err != nil {
+		return
+	}
+	return bytes, nil
 }
 
 func decodeDecimal(r io.Reader) (value Decimal, err error) {
@@ -29,6 +38,7 @@ func decodeDecimal(r io.Reader) (value Decimal, err error) {
 }
 
 func decodeDouble(r io.Reader) (value float64, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
@@ -41,34 +51,49 @@ func decodeFieldTable(r io.Reader) (value map[string]interface{}, err error) {
 }
 
 func decodeFloat(r io.Reader) (value float32, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
-func decodeLongInt(r io.Reader) (value int32, err error) {
+func decodeLongInt(r io.Reader) (value uint32, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
 func decodeLongString(r io.Reader) (value string, err error) {
-	return
+	var length uint32
+	err = binary.Read(r, binary.BigEndian, &length)
+	if err != nil {
+		return
+	}
+	bytes := make([]byte, length)
+	if _, err = io.ReadFull(r, bytes); err != nil {
+		return
+	}
+	return string(bytes), nil
 }
 
-func decodeLongLongInt(r io.Reader) (value int64, err error) {
+func decodeLongLongInt(r io.Reader) (value uint64, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
 func decodeOctet(r io.Reader) (value byte, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
-func decodeShortInt(r io.Reader) (value int16, err error) {
+func decodeShortInt(r io.Reader) (value uint16, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
 func decodeShortShortInt(r io.Reader) (value int8, err error) {
+	err = binary.Read(r, binary.BigEndian, &value)
 	return
 }
 
-func decodeShortStr(r io.Reader) (value string, err error) {
+func decodeShortString(r io.Reader) (value string, err error) {
 	var length uint8
 	err = binary.Read(r, binary.BigEndian, &length)
 	if err != nil {
@@ -82,5 +107,10 @@ func decodeShortStr(r io.Reader) (value string, err error) {
 }
 
 func decodeTimestamp(r io.Reader) (value time.Time, err error) {
-	return
+	var epoch int64
+	err = binary.Read(r, binary.BigEndian, &epoch)
+	if err != nil {
+		return
+	}
+	return time.Unix(epoch, 0), nil
 }
--- decode_test.go
+++ decode_test.go
@@ -5,31 +5,219 @@ import (
 	"io"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestDecodeBoolean(t *testing.T) {
 	cases := []struct {
-		in    []byte
+		in    string
 		value bool
 		err   error
 	}{
-		{[]byte{0x00}, false, nil},
-		{[]byte{0x01}, true, nil},
-		{[]byte{}, false, io.EOF},
+		{"\000", false, nil},
+		{"\001", true, nil},
+		{"", false, io.EOF},
 	}
 	for _, c := range cases {
-		value, err := decodeBoolean(bytes.NewReader(c.in))
+		value, err := decodeBoolean(strings.NewReader(c.in))
 		if err != nil {
 			if err != c.err {
-				t.Errorf("decodeBoolean(%s) == %#v, want %#v", c.in, err, c.err)
+				t.Errorf("decodeLongString(%#v) == %#v, want %#v", c.in, err, c.err)
 			}
-		} else if value != c.value {
-			t.Errorf("decodeBoolean(%v) == %t, want %t", c.in, value, c.value)
+		} else {
+			if value != c.value {
+				t.Errorf("decodeLongString(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
 		}
 	}
 }
 
-func TestDecodeShortStr(t *testing.T) {
+func TestDecodeByteArray(t *testing.T) {
+	cases := []struct {
+		in    string
+		value []byte
+		err   error
+	}{
+		{"\000\000\000\003abc", []byte{0x61, 0x62, 0x63}, nil},
+		{"", []byte{0x00}, io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeByteArray(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeByteArray(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if bytes.Compare(value, c.value) != 0 {
+				t.Errorf("decodeByteArray(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeDouble(t *testing.T) {
+	cases := []struct {
+		in    string
+		value float64
+		err   error
+	}{
+		{"B\002\xa0_ \000\000\000", float64(1e+10), nil},
+		{"", float64(0), io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeDouble(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeDouble(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeDouble(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeFloat(t *testing.T) {
+	cases := []struct {
+		in    string
+		value float32
+		err   error
+	}{
+		{"@I\x0f\xd0", float32(3.14159), nil},
+		{"", float32(0), io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeFloat(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeFloat(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeFloat(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeLongInt(t *testing.T) {
+	cases := []struct {
+		in    string
+		value uint32
+		err   error
+	}{
+		{"\x7f\xff\xff\xff", 2147483647, nil},
+		{"", 0, io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeLongInt(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeLongInt(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeLongInt(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeLongLongInt(t *testing.T) {
+	cases := []struct {
+		in    string
+		value uint64
+		err   error
+	}{
+		{"\x7f\xff\xff\xff\xff\xff\xff\xf8", 9223372036854775800, nil},
+		{"", 0, io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeLongLongInt(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeLongLongInt(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeLongLongInt(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeLongString(t *testing.T) {
+	cases := []struct {
+		in, value string
+		err       error
+	}{
+		{"\000\000\000\n0123456789", "0123456789", nil},
+		{"\000\000\000\014abc123def456", "abc123def456", nil},
+		{"aabc123", "", io.ErrUnexpectedEOF},
+		{"", "", io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeLongString(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeLongString(%s) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeLongString(%s) == %s, want %s", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeOctet(t *testing.T) {
+	cases := []struct {
+		in    string
+		value byte
+		err   error
+	}{
+		{"\xfe", byte(254), nil},
+		{"", byte(0), io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeOctet(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeOctet(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeOctet(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeShortInt(t *testing.T) {
+	cases := []struct {
+		in    string
+		value uint16
+		err   error
+	}{
+		{"\x7f\xff", 32767, nil},
+		{"", 0, io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeShortInt(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeShortInt(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeShortInt(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
+}
+
+func TestDecodeShortString(t *testing.T) {
 	cases := []struct {
 		in, value string
 		err       error
@@ -40,16 +228,38 @@ func TestDecodeShortStr(t *testing.T) {
 		{"", "", io.EOF},
 	}
 	for _, c := range cases {
-		value, err := decodeShortStr(strings.NewReader(c.in))
+		value, err := decodeShortString(strings.NewReader(c.in))
 		if err != nil {
 			if err != c.err {
-				t.Errorf("shortStr(%s) == %#v, want %#v", c.in, err, c.err)
+				t.Errorf("decodeShortString(%#v) == %#v, want %#v", c.in, err, c.err)
 			}
 		} else {
 			if value != c.value {
-				t.Errorf("shortStr(%s) == %s, want %s", c.in, value, c.value)
+				t.Errorf("decodeShortString(%#v) == %#v, want %#v", c.in, value, c.value)
 			}
 		}
 	}
+}
 
+func TestDecodeTimestamp(t *testing.T) {
+	cases := []struct {
+		in    string
+		value time.Time
+		err   error
+	}{
+		{"\x00\x00\x00\x00Ec)\x92", time.Unix(1164126610, 0), nil},
+		{"", time.Unix(0, 0), io.EOF},
+	}
+	for _, c := range cases {
+		value, err := decodeTimestamp(strings.NewReader(c.in))
+		if err != nil {
+			if err != c.err {
+				t.Errorf("decodeTimestamp(%#v) == %#v, want %#v", c.in, err, c.err)
+			}
+		} else {
+			if value != c.value {
+				t.Errorf("decodeTimestamp(%#v) == %#v, want %#v", c.in, value, c.value)
+			}
+		}
+	}
 }
